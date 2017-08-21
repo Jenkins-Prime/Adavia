ec0396af086ba95ef992926796b5f0d00d823429
DESKTOP-85GJ6NR\Joshmond <joshmnd8@gmail.com>
2017-08-21 02:31:46 +0100

    Implemented movement and carea controllers

---
+++ Adavia/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform target;
+    private Camera cam;
+
+    [SerializeField]
+    private float cameraSpeed;
+	void Awake ()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        cam = GetComponent<Camera>();
+	}
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate ()
+    {
+        cam.orthographicSize = (Screen.height / 100.0f) / 2.0f; // Maintain scale for resolution;
+
+        if (target != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, target.position.y, -10), cameraSpeed);
+        }
+	}
+}
---
+++ Adavia/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Animator animator;
+
+    [SerializeField]
+    private float walkingSpeed;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        Movement(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+	}
+
+    private void Movement(float xDirection, float yDirection)
+    {
+
+
+        bool isWalking = Mathf.Abs(xDirection) + Mathf.Abs(yDirection) > 0;
+
+        animator.SetBool("isWalking", isWalking);
+
+        if (isWalking)
+        {
+            animator.SetFloat("xSpeed", xDirection);
+            animator.SetFloat("ySpeed", yDirection);
+
+            transform.position += new Vector3(xDirection, yDirection, 0.0f).normalized * Time.deltaTime * walkingSpeed;
+        }
+
+    }
+}
