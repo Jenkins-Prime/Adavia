5534658b5b88ae1d912b1f03852d0b7fc67a95d3
Jenkins-Prime <ruffdemongames@gmail.com>
2017-10-17 02:47:00 +0800

    Toka Dungeon part 1

---
+++ Adavia/Assets/Scripts/PressureSwitch.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SwitchStates {Timed, Weighted, OneOff}
+
+public class PressureSwitch : MonoBehaviour {
+	public SwitchStates switches;
+	public bool switchActive;
+	private Animator anim;
+	public float switchTimer;
+	private float initialSwitchTimer;
+	private bool timedSwitchActive;
+
+	// Use this for initialization
+	void Start () 
+	{
+		initialSwitchTimer = switchTimer;
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (timedSwitchActive) 
+		{
+			switchTimer -= Time.deltaTime;
+			if (switchTimer <= 0) 
+			{
+				switchTimer = 0;
+				anim.SetBool ("Active", false);
+				timedSwitchActive = false;
+				switchActive = false;
+				switchTimer = initialSwitchTimer;
+				switchActive = false;
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player" || other.tag == "Interactible")
+		{
+			if (switches == SwitchStates.OneOff) 
+			{
+				anim.SetBool ("Active", true);
+				switchActive = true;
+			}
+
+			if (switches == SwitchStates.Weighted) 
+			{
+				anim.SetBool ("Active", true);
+				switchActive = true;
+			}
+				
+			if (switches == SwitchStates.Timed) 
+			{
+				if (switchTimer > 0) 
+				{
+					switchActive = true;
+					timedSwitchActive = true;
+					anim.SetBool ("Active", true);
+				} 
+				else 
+				{
+					return;
+				}
+			}
+		}
+
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (switches == SwitchStates.OneOff) 
+		{
+			return;
+		}
+
+		if (switches == SwitchStates.Weighted) 
+		{
+			anim.SetBool ("Active", false);
+			switchActive = false;
+		}
+			
+	}
+}
---
+++ Adavia/Assets/Scripts/WarpPad.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpPad : MonoBehaviour {
+
+	public GameObject targetWarpPad;
+	public float animTime;
+	private float initialAnimTime;
+	private Animator anim;
+	public float xOffset;
+	public float yOffset;
+	private bool playerOnPad;
+	private PlayerMovement player;
+
+
+	void Start()
+	{
+		anim = GetComponent<Animator> ();
+		initialAnimTime = animTime;
+		player = FindObjectOfType<PlayerMovement> ();
+	}
+	 
+	void Update()
+	{
+		if(playerOnPad)
+
+		animTime--;
+		if (animTime <= 0) 
+		{
+			animTime = 0;
+			player.transform.position = new Vector3 (targetWarpPad.transform.position.x + xOffset, targetWarpPad.transform.position.y + yOffset, transform.position.z);
+			anim.SetBool ("Active", false);
+			animTime = initialAnimTime;
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") 
+		{
+		playerOnPad = true;
+		anim.SetBool ("Active", true);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		playerOnPad = false;
+		anim.SetBool ("Active", false);
+		animTime = initialAnimTime;
+	}
+
+  }
+
