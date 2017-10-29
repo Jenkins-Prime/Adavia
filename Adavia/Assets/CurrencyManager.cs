23865698c45a91bb53da1e1102cc42377eb1a501
Jenkins-Prime <ruffdemongames@gmail.com>
2017-10-30 04:03:34 +0800

    Currency Update

---
+++ Adavia/Assets/CurrencyManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrencyManager : MonoBehaviour {
+	[SerializeField]
+	private int currencyAmount;
+	[SerializeField]
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "" + currencyAmount;
+	}
+
+	public void AddCurrency(int amountToAdd)
+	{
+		currencyAmount += amountToAdd;
+	}
+}
---
+++ Adavia/Assets/CurrencyPickup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrencyPickup : MonoBehaviour {
+
+	[SerializeField]
+	private int valueAmount;
+	private CurrencyManager cMan;
+
+	// Use this for initialization
+	void Start () {
+		cMan = FindObjectOfType<CurrencyManager> ();
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") 
+		{
+			cMan.AddCurrency (valueAmount);
+			Destroy (gameObject);
+		}
+	}
+}
